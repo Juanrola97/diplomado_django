e83d7946a1b1fa7da72d739d6023554e82238acc
Juan Rodriguez <juan.rodriguez@ciberdix.com>
2021-03-20 11:45:08 -0500

    helloworld first commit

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+def HelloWorld():
+  print("Hello World!")
+def Suma(a,b):
+      print(a+b)
+def Resta(a,b):
+    print(a-b)
+def Multiplica(a,b):
+    print(a*b)
+def Division(a,b):
+    print(a/b)
+
+HelloWorld()
+Suma(4, 5)
+Resta(10, 20)
+Multiplica(30, 20)
+Division(10, 20)
