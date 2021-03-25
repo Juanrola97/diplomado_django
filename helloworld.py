6466d3cd1d4083e15e3c0df83ab037dbdfcc3b58
Juan Rodriguez <juan.rodriguez@ciberdix.com>
2021-03-24 20:59:58 -0500

    avances de la clase 24/03/2021

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,51 @@
+
+# Funciones basicas primera parte
 def HelloWorld():
   print("Hello World!")
-def Suma(a,b):
+def suma(a,b):
       print(a+b)
-def Resta(a,b):
+def resta(a,b):
     print(a-b)
-def Multiplica(a,b):
+def multiplica(a,b):
     print(a*b)
-def Division(a,b):
+def division(a,b):
     print(a/b)
+def potencia(a,b):
+    return a**b
+def saludo(nombre):
+    print("Hola " + nombre)
+def despedida(nombre):
+    print("Adios " + nombre)
+
 
 HelloWorld()
-Suma(4, 5)
-Resta(10, 20)
-Multiplica(30, 20)
-Division(10, 20)
+suma(5, 5)
+resta(10, 20)
+multiplica(30, 20)
+division(10, 20)
+potencia(30, 20)
+saludo("Juan")
+despedida("Juan")
+
+
+# Funcion compleja
+
+# Funcion para calcular el pago de cesantias
+def calcularCesantias(diasLaborados, salario, diasAgno):
+    return (diasLaborados*salario)/diasAgno
+
+diasLaborados = 191
+salario = 1500000
+diasAgno = 360
+print("El valor de tus cesantias son: " + str(calcularCesantias(diasLaborados, salario, diasAgno)))
+
+# Funcion para calcular posibles ahorros
+def calcularPosibleAhorros(salario, gastos, porcentajeAhorrar):
+    return (salario-gastos) * porcentajeAhorrar
+
+salario = 1000000
+gastos = 700000
+porcentajeAhorrar = 0.5
+print("Tus ahorros deseados son: " + str(calcularPosibleAhorros(salario, gastos,porcentajeAhorrar)))
+
+    
