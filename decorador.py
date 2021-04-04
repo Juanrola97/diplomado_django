cde12f1e84492ff0c8b570296b69b63a709eb4af
Juan Rodriguez <juanrola97@gmail.com>
2021-04-04 16:58:22 -0500

    avances taller

--- decorador.py
+++ decorador.py
@@ -15,3 +15,25 @@ def dummy_code(nota_1, nota_2):
   print(prom)
 
 dummy_code(5, 0)
+
+
+# Error de prevencion indice de una lista que no existe #
+
+@prevet_error
+def dummy_code2(lista):
+  respuesta = lista[10]
+  print(respuesta)
+
+lista = ['hola','mundo']
+dummy_code2(lista)
+
+# Error de prevencion de un valor null #
+
+
+@prevet_error
+def dummy_code3(valor):
+  respuesta = valor*valor
+  print(respuesta)
+
+valor = None
+dummy_code3(valor)
