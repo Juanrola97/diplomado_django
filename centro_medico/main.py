ee5c92ed0369ec41f243eec39eee247c6a7c2e07
Juan Rodriguez <juan.rodriguez@ciberdix.com>
2021-04-07 21:09:11 -0500

    creacion de main en la carpeta centro medico

---
+++ centro_medico/main.py
@@ -0,0 +1,24 @@
+'''
+2) Implementar la logica de una institucion de servicios medicos.
+
+    Doctores
+    Pacientes
+    Citas
+    Notas medicas
+    Horarios de citas (*opcional)
+
+estas entidades deben ser implementadas mediante objetos. el programa debe permitir las siguientes funciones:
+
+1) un doctor debe poder ver las citas que tienen asignadas (un doctor puede atender varias citas al dia pero debe al menos dar una y no debe tener mas de 10 horas de trabajo)
+
+2) cada cita debe tener un paciente y un doctor
+
+3) un paciente puede tener inscritas en muchas citas
+
+4) las notas medicas son ingresadas por el doctor que atiende la cita medica con en paciente tiene asignado
+
+5) los paciente deben poder ver un historial de notas medicas escritas por cada doctor
+
+6) {OPCIONAL} - los horarios son definidos de maximo 12 horas diarias en horario de oficina, un paciente no puede estar en dos citas con el mismo horario, un doctor no puede atender dos citas medicas que sean vistas el mismo tiempo
+
+'''
